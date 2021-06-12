d5b3ffc4059047a9dc80f34b5abc890be20d7274
livin2 <livin2@qq.com>
2021-06-12 13:56:32 +0800

    add eruda

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-
+import eruda from 'eruda'
 Vue.config.productionTip = false
-
+eruda.init()
 new Vue({
   render: h => h(App),
 }).$mount('#app')
