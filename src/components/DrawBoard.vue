bdcbfd551298b7ed05a13da7d36e8cd5acf7dc5b
livin2 <livin2@qq.com>
2021-06-14 18:25:51 +0800

    ✨ feat: 路由表UI

--- src/components/DrawBoard.vue
+++ src/components/DrawBoard.vue
@@ -1,91 +1,111 @@
 <template>
-<div>
-  <a-affix :offset-top="10"
-     :style="{ position: 'absolute', top: '10px', right: '10px','z-index':'999'}">
-      <a-button type="primary" @click="addRoute">
-        添加节点
-      </a-button>
-      <a-button type="primary" style="margin-left:10px"
-        v-if='selectionStack[0] && selectionStack[1]&&canLink'
-        @click="linkRoute">
+  <div>
+    <a-affix
+      :offset-top="10"
+      :style="{
+        position: 'absolute',
+        top: '10px',
+        right: '10px',
+        'z-index': '999',
+      }"
+    >
+      <a-button type="primary" @click="addRoute"> 添加节点 </a-button>
+      <a-button
+        type="primary"
+        style="margin-left: 10px"
+        v-if="selectionStack[0] && selectionStack[1] && canLink"
+        @click="linkRoute"
+      >
         连接路由
       </a-button>
     </a-affix>
-<div id="main">
-  <div id="container" ref="container"></div>
-</div>
-</div>
+    <RouteTable/>
+    <div id="main">
+      <div id="container" ref="container"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { Graph} from "@antv/x6";
-import api from "../api"
+import { Graph } from "@antv/x6";
+import RouteTable from './RouteTable.vue'
+import api from "../api";
 export default {
   name: "DrawBoard",
+  components: {
+    RouteTable
+  },
   props: {
     msg: String,
   },
-  data(){
-    return{
-      graph:null,
-      canLink:false,
-      routeLabelSta:0,
-      selectionStack:[null,null],
-    }
+  data() {
+    return {
+      currentRouteTable: [],
+      graph: null,
+      canLink: false,
+      routeLabelSta: 0,
+      selectionStack: [null, null],
+    };
   },
-  methods:{
-    addRoute(){
-      let rcolor = (Math.floor(Math.random()*128+127)<<16|Math.floor(Math.random()*128+127)<<8|Math.floor(Math.random()*128+127)).toString(16);
+  methods: {
+    addRoute() {
+      let rcolor = (
+        (Math.floor(Math.random() * 128 + 127) << 16) |
+        (Math.floor(Math.random() * 128 + 127) << 8) |
+        Math.floor(Math.random() * 128 + 127)
+      ).toString(16);
       const node = this.graph.addNode({
-        shape: 'rect', // 指定使用何种图形，默认值为 'rect'
+        shape: "rect", // 指定使用何种图形，默认值为 'rect'
         x: 40,
         y: 40,
         width: 80,
         height: 40,
         label: `R${this.routeLabelSta}`,
-        attrs:{
+        attrs: {
           body: {
-            fill:`#${rcolor}`  // 背景颜色
+            fill: `#${rcolor}`, // 背景颜色
           },
-        }
-      })
+        },
+      });
       this.routeLabelSta++;
       // node['ConnectedRoutes'] = {}
       // node.getRid = function(){return this.label} //OR return this.id
-      api.onAddRoute(node)
+      api.onAddRoute(node);
     },
-    onRouteSelected(node){
-      this.selectionStack.push(node)
+    onRouteSelected(node) {
+      this.selectionStack.push(node);
       let pre = this.selectionStack.shift();
-      if(pre) this.graph.unselect(pre);
-      if(this.selectionStack[0] && this.selectionStack[1]){
-        if(!this.graph.isNeighbor(this.selectionStack[0],this.selectionStack[1]))
+      if (pre) this.graph.unselect(pre);
+      if (this.selectionStack[0] && this.selectionStack[1]) {
+        if (
+          !this.graph.isNeighbor(this.selectionStack[0], this.selectionStack[1])
+        )
           this.canLink = true;
       }
-      console.info('onRouteSelected',this.selectionStack)
+      console.info("onRouteSelected", this.selectionStack);
     },
-    linkRoute(){
+    linkRoute() {
       let selected = this.graph.getSelectedCells();
-      if(this.graph.isNeighbor(selected[0],selected[1]))return;
+      if (this.graph.isNeighbor(selected[0], selected[1])) return;
       this.graph.addEdge({
-        shape: 'edge', // 指定使用何种图形，默认值为 'edge'
+        shape: "edge", // 指定使用何种图形，默认值为 'edge'
         source: selected[0],
         target: selected[1],
-        attrs:{
-          line:{targetMarker:''}
+        attrs: {
+          line: { targetMarker: "" },
         },
         tools: {
-          name: 'button-remove',
+          name: "button-remove",
         },
-      })
+      });
       this.canLink = false;
-      api.onLinkRoutes(selected[0],selected[1]);
-    }
+      api.onLinkRoutes(selected[0], selected[1]);
+    },
   },
   mounted() {
     this.graph = new Graph({
       container: this.$refs.container,
-      autoResize:window.Docuemnt,
+      autoResize: window.Docuemnt,
       grid: {
         size: 10, // 网格大小 10px
         visible: true, // 渲染网格背景
@@ -93,15 +113,19 @@ export default {
       selecting: {
         enabled: true,
         rubberband: true, // 启用框选
-        showNodeSelectionBox:true,
-        filter: ['edge']
+        showNodeSelectionBox: true,
+        filter: ["edge"],
       },
     });
-    this.graph.on('node:selected', ({node}) => this.onRouteSelected(node))
-    this.graph.on('node:unselected',()=>this.graph.isSelectionEmpty()?this.selectionStack=[null,null]:'');
-    this.graph.on("edge:removed", ({ edge}) => {
-      api.onDisconnect(this.graph.getCellById(edge.getSourceCellId()),
-        this.graph.getCellById(edge.getTargetCellId()));
+    this.graph.on("node:selected", ({ node }) => this.onRouteSelected(node));
+    this.graph.on("node:unselected", () =>
+      this.graph.isSelectionEmpty() ? (this.selectionStack = [null, null]) : ""
+    );
+    this.graph.on("edge:removed", ({ edge }) => {
+      api.onDisconnect(
+        this.graph.getCellById(edge.getSourceCellId()),
+        this.graph.getCellById(edge.getTargetCellId())
+      );
     });
   },
 };
@@ -109,8 +133,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-#main{
-  width:100vw;
+#main {
+  width: 100vw;
   height: 100vh;
   position: absolute;
   top: 0;
@@ -119,15 +143,15 @@ export default {
   flex-wrap: nowrap;
   align-items: stretch;
 }
-#container{
+#container {
   height: auto;
   flex: 1;
 }
-::v-deep .x6-widget-selection-box{
-    // border:none;
-    margin: 0;
-    padding: 0;
-    box-shadow: 0px 0px 9px 4px #5498f1;
-    border: none;
+::v-deep .x6-widget-selection-box {
+  // border:none;
+  margin: 0;
+  padding: 0;
+  box-shadow: 0px 0px 9px 4px #5498f1;
+  border: none;
 }
 </style>
---
+++ src/components/RouteTable.vue
@@ -0,0 +1,65 @@
+<template>
+  <a-affix
+    :offset-bottom="10"
+    :style="{
+      position: 'absolute',
+      bottom: '10px',
+      left: '10px',
+      'z-index': '999',
+    }"
+  >
+    <a-popover trigger="click" placement="topLeft">
+      <template slot="content">
+        <a-table
+          style="width: 30vw"
+          :columns="columns"
+          :data-source="data"
+          bordered
+          :pagination="{ pageSize: 50 }"
+          :scroll="{ y: 240 }"
+        >
+          <template slot="name" slot-scope="text">
+            <a>{{ text }}</a>
+          </template>
+        </a-table>
+      </template>
+      <a-button type="primary"> 路由表 </a-button>
+    </a-popover>
+  </a-affix>
+</template>
+<script>
+const columns = [
+  {
+    title: "目标",
+    dataIndex: "to",
+  },
+  {
+    title: "距离",
+    dataIndex: "dist",
+  },
+  {
+    title: "下一跳",
+    dataIndex: "next",
+  },
+];
+
+const data = [];
+for (let i = 0; i < 100; i++) {
+  data.push({
+    key: i,
+    to: `K${i}`,
+    dist: 32,
+    next: `L${i}`,
+  });
+}
+
+export default {
+  name: "RouteTable",
+  data() {
+    return {
+      data,
+      columns,
+    };
+  },
+};
+</script>
