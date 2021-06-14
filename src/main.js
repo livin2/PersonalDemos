f068c24b0d06f264c7eda75e3f6a8c93ddcc73bc
livin2 <livin2@qq.com>
2021-06-14 14:59:35 +0800

    🐳 chore(main.js): add antv

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
 import eruda from 'eruda'
 Vue.config.productionTip = false
+Vue.use(Antd);
 eruda.init()
 new Vue({
   render: h => h(App),
