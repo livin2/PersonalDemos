17576b2a9ddb8ee05408bcb3b017ee7213268ab8
livin2 <361473023@qq.com>
2022-04-29 05:37:48 +0000

    Deploying to gh-pages from @ livin2/PersonalDemos@479edad6e15167ef97c394e6f77c7e240954a871 🚀

---
+++ precache-manifest.72fa662262a7127a589ab2abbff3066a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ee8f3599c103feb4c4",
+    "url": "css/app.4e33a024.css"
+  },
+  {
+    "revision": "01f84f957c596852dc57",
+    "url": "css/chunk-vendors.f0c21e17.css"
+  },
+  {
+    "revision": "68075b9e5c166aaf9ffbef7d16d924da",
+    "url": "index.html"
+  },
+  {
+    "revision": "d3ee8f3599c103feb4c4",
+    "url": "js/app.0dcba84e.js"
+  },
+  {
+    "revision": "01f84f957c596852dc57",
+    "url": "js/chunk-vendors.cb15b8f7.js"
+  },
+  {
+    "revision": "a0611d0a26ead07d1f1ce5ea9021f38c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
